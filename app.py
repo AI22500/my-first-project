2c5bfb3c0f1007304a66e4666427724f19283aa9
AI22500 <ren0209215@gmail.com>
2026-02-12 13:48:57 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from supabase import create_client
+import os
+
+url = os.environ["SUPABASE_URL"]
+key = os.environ["SUPABASE_KEY"]
+
+supabase = create_client(url, key)
 import streamlit as st
 
 PAGE_TITLE = "Todoリスト"
